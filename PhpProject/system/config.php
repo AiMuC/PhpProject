9d255a3aadbf94cc6a3ab302c3b99c9a99c6c18f
AiMuC <31570354+AiMuC@users.noreply.github.com>
2021-02-12 16:21:11 +0800

    Update config.php

--- PhpProject/system/config.php
+++ PhpProject/system/config.php
@@ -8,7 +8,7 @@
 */
 $config = array(
     'host' => 'localhost',
-    'username' => 'root',
-    'password' => 'password',
-    'dbname' => 'xypp'
+    'username' => '数据库用户名',
+    'password' => '数据库密码',
+    'dbname' => '数据库名称'
 );
